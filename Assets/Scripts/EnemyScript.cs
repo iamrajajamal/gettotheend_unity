90cafcba6008f4b6d563595ab5bd7ef31bf48eba
Raja Jamal Parvez <rajajamalparvez@gmail.com>
2019-04-22 19:32:22 +0500

    Added Player and Enemy with Script

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+    [SerializeField]
+    private bool moveLeft;
+
+    void Update()
+    {
+        if (moveLeft)
+        {
+            Vector3 temp = transform.position;
+            temp.x -= moveSpeed * Time.deltaTime;
+            transform.position = temp;
+        }
+        else
+        {
+            Vector3 temp = transform.position;
+            temp.x += moveSpeed * Time.deltaTime;
+            transform.position = temp;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Bounce")
+        {
+            moveLeft = !moveLeft;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float moveSpeed = 1f;
+
+    private void FixedUpdate()
+    {
+        Vector2 temp = transform.position;
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            temp.x += moveSpeed * Time.deltaTime;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            temp.x -= moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            temp.y += moveSpeed * Time.deltaTime;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            temp.y -= moveSpeed * Time.deltaTime;
+        }
+
+        transform.position = temp;
+    }
+}
